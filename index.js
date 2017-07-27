bb8fd3689c0e5a0087463ba6b246499884ad669f
Jannis R <mail@jannisr.de>
2017-07-27 15:16:33 +0200

    use process.platform to check for Linux

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var spawn = require('child_process').spawn
 var os = require('os')
 var Through = require('audio-through')
 
-if (os.type() === 'Darwin' || os.type().indexOf('Windows') > -1) {
+if (process.platform !== 'linux') {
   throw new Error('Only linux is supported with Node -- alas! Please file a PR!')
 }
 
