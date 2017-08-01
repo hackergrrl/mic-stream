758ce106af9c01cb4ac9e05c7259c40df56c4ecc
Stephen Whitmore <sww@eight45.net>
2017-07-31 17:05:56 -0700

    use tape for tests

--- test.js
+++ test.js
@@ -1,16 +1,30 @@
 'use strict'
 
-var assert = require('assert')
+var test = require('tape')
 var isStream = require('is-stream')
 var micStream = require('.')
+var Through = require('audio-through')
 
-var s = micStream()
+test('can start, is a stream, can stop', function (t) {
+  t.plan(2)
 
-assert.ok(isStream.readable(s), 'does not return a readable stream')
-
-s.on('data', (buf) => {
-  // todo: check if buf is an AudioBuffer
-  assert.ok(buf.byteLength > 0, 'buffer is empty')
+  var s = micStream()
+  t.ok(isStream.readable(s), 'returns a readable stream')
+  s.stop(function () {
+    t.ok(true)
+  })
 })
 
-s.once('data', () => s.stop())
+test('emits AudioBuffer objects', function (t) {
+  t.plan(3)
+
+  var s = micStream()
+  s.pipe(Through())
+  s.once('data', function (buf) {
+    t.ok(buf.constructor.name === 'AudioBuffer', 'isn\'t an AudioBuffer object')
+    t.ok(buf.data.length > 0, 'buffer is empty')
+    s.stop(function () {
+      t.ok(true)
+    })
+  })
+})
